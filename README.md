1da51873dac4798f473f7d746e148db80dcd3b02
TheFaded <gregory.lui23@ycdsbk12.ca>
2021-01-14 09:53:24 -0500

    commit 1

--- README.md
+++
@@ -1,4 +0,0 @@
-Actual Repl.it link: https://repl.it/@GregoryLui/ICS20-CPT-Gregory-Lui
-Fork 1: https://repl.it/@GregoryLui/ICS20-CPT-Gregory-Lui-1
-# Pygame Workflow
-![PygameLoop.png](PygameLoop.png)
--- main.py
+++ main.py
@@ -13,32 +13,42 @@ RED      = ( 255,   0,   0)
 pygame.init()
   
 # Set the width and height of the screen [width, height]
-size = (700, 500)
+size = [800, 800]
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
  
+PLAYER_X = 0
+PLAYER_Y = 0
 #Loop until the user clicks the close button.
-done = False
+DONE = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
-while not done:
+while not DONE:
     # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-  
+            DONE = True # Flag that we are DONE so we exit this loop
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                PLAYER_Y = PLAYER_Y + 50
+            elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                PLAYER_Y = PLAYER_Y - 50
+            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                PLAYER_X = PLAYER_X - 50
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                PLAYER_X = PLAYER_X + 50
+            print(PLAYER_X, PLAYER_Y)
     # --- Game logic should go here
  
     # --- Drawing code should go here
-     
     # First, clear the screen to white or whatever background colour. 
     # Don't put other drawing commands above this, or they will be erased with this command.
     screen.fill(WHITE)
-     
+    pygame.draw.rect(screen, RED, ([PLAYER_X, PLAYER_Y], [50, 50]))    
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
